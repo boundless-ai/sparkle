bc74d0e73ecc1b449ddfd101f8c26fd8a3d734a4
Ronith Kandallu <ronithkandallu@gmail.com>
2023-10-04 15:58:46 -0700

    Update SPM file for version 2.5.0-patch

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@
 import PackageDescription
 
 // Version is technically not required here, SPM doesn't check
-let version = "2.5.0"
+let version = "2.5.0-patch"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.5.0"
-let checksum = "f7e6ef91edee5704b76358c9485edc63a79dfefe770d8b5340818b7db69187df"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
+let tag = "2.5.0-patch"
+let checksum = "3f836be9542a21f11e97dc51e02778f6cca873fb3e237c0a33f8a5991a274813"
+let url = "https://github.com/boundless-ai/sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
